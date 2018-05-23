f1f847910900494cc1e2fc75f6fc7e84fc8aeaa6
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-05-23 13:26:33 -0400

    feat: Built stock.js route that pulls a JSON object from alpha vantage API for stock data

--- server/routes/api/stock.js
+++ server/routes/api/stock.js
@@ -2,17 +2,36 @@ const request = require('superagent');
 
 module.exports = (app) => {
     app.post('/api/stock/portfolio', function (req, res, next) {
-
-        const tickers = [];
+        const apiKey = 'WO3CN6DU2D02X0QH';
+        const tickers = ['MSFT'];
 
         // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=WO3CN6DU2D02X0QH
-        request
-           .get('/search')
-           .query({ query: 'Manny' })
-           .query({ range: '1..5' })
-           .query({ order: 'desc' })
-           .then(function(res) {
+        let completed = 0;
+        const results = [];
+        for (let i = 0; i < tickers.length; i += 1) {
+            const ticker = tickers[i];
+            request
+               .get('https://www.alphavantage.co/query')
+               .query({ 'function': 'TIME_SERIES_DAILY' })
+               .query({ symbol: ticker })               
+               .query({ apikey: apiKey })
+               // .then(res => res.json())
+               .then((response) => {
+                    completed += 1;
+                    //console.log('res', res.body['Meta Data']);
+                    results.push(response.body);
+                    if (completed === tickers.length) {
+                        //all tickers have finished their requests
 
-        });
+                        console.log('completed')
+
+                        res.send({
+                            success: true,
+                            message: 'Ticker info',
+                            results: results
+                        });
+                    }
+            });
+        }
     });
 };
