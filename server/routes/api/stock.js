57ed34e5bc3785dea56961289c520df1883a8a26
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-05-25 13:57:58 -0400

    bug: Remove extra stocks

--- server/routes/api/stock.js
+++ server/routes/api/stock.js
@@ -4,7 +4,7 @@ const request = require('superagent');
 module.exports = (app) => {
     app.post('/api/stock/portfolio', function (req, res, next) {
         const apiKey = 'WO3CN6DU2D02X0QH';
-        const tickers = ['MSFT', 'SSYS', 'AAPL', 'NWL', 'CAT'];
+        const tickers = ['MSFT'];
 
         // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=WO3CN6DU2D02X0QH
         let completed = 0;
